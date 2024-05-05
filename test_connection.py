35e32b4e31d8dddb0b18000e41396a95a94fcc39
Greg Suk <gsuk@phdata.io>
2024-05-05 14:56:58 +0000

    made changes to lab instructions and updating connections to streamlit

--- test_connection.py
+++ test_connection.py
@@ -2,14 +2,14 @@ import configparser
 import os
 import sys
 from snowflake.snowpark import Session
-from utils.snowpark_utils import get_snowsql_config
+# from utils.snowpark_utils import get_snowsql_config
 from snowflake.connector.errors import DatabaseError, OperationalError
 
 if __name__ == "__main__":
     current_dir = os.getcwd()
     parent_dir = os.path.dirname(current_dir)
     sys.path.append(parent_dir)
-    snowpark_config = get_snowsql_config()
+    snowpark_config = Session.builder.getOrCreate()
     snowpark_config['ROLE'] = 'ACCOUNTADMIN'
 
     try:
